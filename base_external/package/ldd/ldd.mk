c1f578b59b37800efa7d55c2a46be3d610751e78
Florian Depraz <florian.depraz@outlook.com>
2025-04-02 14:52:21 +0000

    Add driver from lab3 updated
    Signed-off-by: Florian Depraz <florian.depraz@outlook.com>

--- base_external/package/ldd/ldd.mk
+++ base_external/package/ldd/ldd.mk
@@ -1,28 +1,29 @@
-LDD_VERSION = 5c3cae6ddc96b8645dfa6f6bc4ddbba08aae8789
-LDD_SITE = 'https://github.com/cu-ecen-aeld/assignment-7-pokitoz'
+LDD_VERSION = caac8d3954d20f43643ba0e2ba3087ec2824f0d0
+LDD_SITE = 'https://github.com/cu-ecen-aeld/assignments-3-and-later-pokitoz'
 LDD_SITE_METHOD = git
 LDD_GIT_SUBMODULES = YES
 
-#define LDD_BUILD_CMDS
-#	ARCH=arm64 KDIR=$(BUILD_DIR)/linux-$(BR2_LINUX_KERNEL_VERSION) $(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/misc-modules
-#	ARCH=arm64 KDIR=$(BUILD_DIR)/linux-$(BR2_LINUX_KERNEL_VERSION) $(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/scull
-#endef
+LDD_MODULE_SUBDIRS = aesd-char-driver
 
-LDD_MODULE_SUBDIRS = misc-modules
-LDD_MODULE_SUBDIRS += scull
 
-#define LDD_INSTALL_TARGET_CMDS
-#	$(INSTALL) -m 0755 $(@D)/misc-modules/module_load $(TARGET_DIR)/usr/bin/
-#	$(INSTALL) -m 0755 $(@D)/misc-modules/module_unload $(TARGET_DIR)/usr/bin/
-#	$(INSTALL) -m 0755 $(@D)/scull/scull_load $(TARGET_DIR)/usr/bin/
-#	$(INSTALL) -m 0755 $(@D)/scull/scull_unload $(TARGET_DIR)/usr/bin/
-#endef
+define LDD_BUILD_CMDS
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/finder-app all
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/server all
+
+endef
 
 define LDD_INSTALL_TARGET_CMDS
- 	$(INSTALL) -m 0755 $(@D)/scull/scull_load $(TARGET_DIR)/usr/bin
- 	$(INSTALL) -m 0755 $(@D)/scull/scull_unload $(TARGET_DIR)/usr/bin
- 	$(INSTALL) -m 0755 $(@D)/misc-modules/module_load $(TARGET_DIR)/usr/bin
- 	$(INSTALL) -m 0755 $(@D)/misc-modules/module_unload $(TARGET_DIR)/usr/bin
+ 	$(INSTALL) -m 0755 $(@D)/aesd-char-driver/aesdchar_load $(TARGET_DIR)/usr/bin
+ 	$(INSTALL) -m 0755 $(@D)/aesd-char-driver/aesdchar_unload $(TARGET_DIR)/usr/bin
+
+	$(INSTALL) -d 0755 $(@D)/conf/ $(TARGET_DIR)/etc/finder-app/conf/
+	$(INSTALL) -m 0755 $(@D)/conf/* $(TARGET_DIR)/etc/finder-app/conf/
+
+	$(INSTALL) -m 0755 $(@D)/server/aesdsocket $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/server/aesdsocket-start-stop $(TARGET_DIR)/etc/init.d/S99aesdsocket
+
+	$(INSTALL) -m 0755 $(@D)/assignment-autotest/test/assignment8/* $(TARGET_DIR)/bin
+
 endef
 
 $(eval $(kernel-module))
