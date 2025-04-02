c76022eaea24bdd61b30db84a4e83720026841ac
Florian Depraz <florian.depraz@outlook.com>
2025-04-02 15:30:03 +0000

    Load module aesd when booting
    Signed-off-by: Florian Depraz <florian.depraz@outlook.com>

--- base_external/package/ldd/ldd.mk
+++ base_external/package/ldd/ldd.mk
@@ -1,4 +1,4 @@
-LDD_VERSION = caac8d3954d20f43643ba0e2ba3087ec2824f0d0
+LDD_VERSION = 60b6b9160d7477f734d59111ab66925c3ca04625
 LDD_SITE = 'https://github.com/cu-ecen-aeld/assignments-3-and-later-pokitoz'
 LDD_SITE_METHOD = git
 LDD_GIT_SUBMODULES = YES
@@ -6,21 +6,9 @@ LDD_GIT_SUBMODULES = YES
 LDD_MODULE_SUBDIRS = aesd-char-driver
 
 
-define LDD_BUILD_CMDS
-	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/finder-app all
-	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/server all
-
-endef
-
 define LDD_INSTALL_TARGET_CMDS
  	$(INSTALL) -m 0755 $(@D)/aesd-char-driver/aesdchar_load $(TARGET_DIR)/usr/bin
  	$(INSTALL) -m 0755 $(@D)/aesd-char-driver/aesdchar_unload $(TARGET_DIR)/usr/bin
-
-	$(INSTALL) -d 0755 $(@D)/conf/ $(TARGET_DIR)/etc/finder-app/conf/
-	$(INSTALL) -m 0755 $(@D)/conf/* $(TARGET_DIR)/etc/finder-app/conf/
-
-	$(INSTALL) -m 0755 $(@D)/server/aesdsocket $(TARGET_DIR)/usr/bin
-	$(INSTALL) -m 0755 $(@D)/server/aesdsocket-start-stop $(TARGET_DIR)/etc/init.d/S99aesdsocket
 
 	$(INSTALL) -m 0755 $(@D)/assignment-autotest/test/assignment8/* $(TARGET_DIR)/bin
 
