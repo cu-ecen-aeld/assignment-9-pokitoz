42de912029ca7e290fab0eda6c66b6805fa4f4e1
Florian Depraz <florian.depraz@outlook.com>
2025-04-03 15:29:13 +0000

    Assignment 9
    Signed-off-by: Florian Depraz <florian.depraz@outlook.com>

--- base_external/package/ldd/ldd.mk
+++ base_external/package/ldd/ldd.mk
@@ -1,4 +1,4 @@
-LDD_VERSION = 60b6b9160d7477f734d59111ab66925c3ca04625
+LDD_VERSION = 6595d47b9b3c94a7c7bcfaa4d6cd34bfb9be3bd1
 LDD_SITE = 'https://github.com/cu-ecen-aeld/assignments-3-and-later-pokitoz'
 LDD_SITE_METHOD = git
 LDD_GIT_SUBMODULES = YES
